d2613e2bde727d636a2ded59aab16b421a864de7
NGPixel <github@ngpixel.com>
2026-02-06 17:47:18 -0500

    feat: add auto refresh

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,24 @@
           </div>
           <div class="hidden lg:ml-4 lg:block">
             <div class="flex items-center">
+              <svg
+                v-show="state.isLoading"
+                class="mr-3 -ml-1 size-5 animate-spin text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24">
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
               <a
                 href="https://ietf.cal.com/side-meetings"
                 target="_blank"
@@ -246,6 +264,7 @@ import { computed, reactive } from 'vue'
 import { DateTime } from 'luxon'
 
 const state = reactive({
+  isLoading: false,
   meeting: {
     meetingNumber: '---',
     meetingLocation: '---',
@@ -332,6 +351,10 @@ function showDetails(booking) {
 }
 
 async function fetchData() {
+  if (state.isLoading) {
+    return
+  }
+  state.isLoading = true
   try {
     const resp = await fetch('/_data').then((r) => r.json())
     if (state.firstFetch) {
@@ -365,7 +388,9 @@ async function fetchData() {
   } catch (err) {
     console.warn(err)
   }
+  state.isLoading = false
 }
 
+setInterval(fetchData, 60000)
 fetchData()
 </script>
